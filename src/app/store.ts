de275652fbdf62f0cc565ed3b4f427eedd6b5d93
AndyHsieh <andy.hsieh@gaiatechs.com>
2024-08-28 14:57:06 +0800

    Redux Essentials, Part 3: Basic Redux Data Flow

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit"
+import postsReducer from '@/features/posts/postsSlice'
+
+export const store = configureStore({
+  // Pass in the root reducer setup as the `reducer` argument
+  reducer: {
+    // Declare that `state.counter` will be updated by the `counterReducer` function
+    posts: postsReducer
+  }
+})
+
+// Infer the type of `store`
+export type AppStore = typeof store
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = typeof store.dispatch
+// Same for the `RootState` type
+export type RootState = ReturnType<typeof store.getState>
