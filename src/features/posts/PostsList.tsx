43ff0a4fa153a2980ac4c1a22e2e3c0e29416e08
AndyHsieh <andy.hsieh@gaiatechs.com>
2024-09-02 17:30:19 +0800

    More Post Features - Post Reaction Buttons - Showing Reaction Buttons

--- src/features/posts/PostsList.tsx
+++ src/features/posts/PostsList.tsx
@@ -2,17 +2,20 @@ import { Link } from 'react-router-dom'
 import { useAppSelector } from '@/app/hooks'
 import { selectAllPosts } from './postsSlice'
 import { PostAuthor } from './PostAuthor'
+import { ReactionButtons } from './ReactionButtons'
 
 export const PostsList = () => {
   // Select the `state.posts` value from the store into the component
   const posts = useAppSelector(selectAllPosts)
+  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-  const renderedPosts = posts.map(post => (
+  const renderedPosts = orderedPosts.map(post => (
     <article className="post-excerpt" key={post.id}>
       <h3>
         <Link to={`/posts/${post.id}`}>{post.title}</Link>
       </h3>
       <PostAuthor userId={post.user} />
+      <ReactionButtons post={post} />
       <p className="post-content">{post.content.substring(0, 100)}</p>
     </article>
   ))
--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,20 +1,53 @@
 import { RootState } from '@/app/store'
 import { PayloadAction, createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
-// Define a TS type for the data we'll be using
+export interface Reactions {
+  thumbsUp: number
+  tada: number
+  heart: number
+  rocket: number
+  eyes: number
+}
+
+export type ReactionName = keyof Reactions
+
 export interface Post {
   id: string
   title: string
   content: string
   user: string
+  date: string
+  reactions: Reactions
 }
 
 type PostUpdate = Pick<Post, 'id' | 'title' | 'content'>
 
-// Create an initial state value for the reducer, with that type
+const initialReactions: Reactions = {
+  thumbsUp: 0,
+  tada: 0,
+  heart: 0,
+  rocket: 0,
+  eyes: 0
+}
+
 const initialState: Post[] = [
-  { id: '1', title: 'First Post!', content: 'Hello!', user: '0' },
-  { id: '2', title: 'Second Post', content: 'More text', user: '2' }
+  {
+    id: '1',
+    title: 'First Post!',
+    content: 'Hello!',
+    user: '0',
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: initialReactions
+  },
+  {
+    id: '2',
+    title: 'Second Post',
+    content: 'More text',
+    user: '2',
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: initialReactions
+  }
 ]
 
 // Create the slice and pass in the initial state
@@ -30,7 +63,14 @@ const postsSlice = createSlice({
       // "prepare callback" function can take multiple arguments, generate random values like unique IDs, and run whatever other synchronous logic is needed to decide what values go into the action object.
       prepare(title: string, content: string, userId: string) {
         return {
-          payload: { id: nanoid(), title, content, user: userId }
+          payload: {
+            id: nanoid(),
+            date: new Date().toISOString(),
+            title,
+            content,
+            user: userId,
+            reactions: initialReactions
+          }
         }
       }
     },
@@ -42,7 +82,18 @@ const postsSlice = createSlice({
         updatedPost.title = title
         updatedPost.content = content
       }
+    },
+    reactionAdded(
+      state,
+      action: PayloadAction<{ postId: string; reaction: ReactionName }>
+    ) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
     }
+
   },
   selectors: {
     // Note that these selectors are given just the `PostsState`
@@ -55,7 +106,7 @@ const postsSlice = createSlice({
 
 })
 
-export const { postAdded, postUpdated } = postsSlice.actions
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 // replaced standalone selectors with these:
 export const { selectAllPosts, selectPostById } = postsSlice.selectors
