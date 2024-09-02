259f4a890be4c0cc5a21c27c388742b3854b20cd
AndyHsieh <andy.hsieh@gaiatechs.com>
2024-09-02 16:25:06 +0800

    Users and Posts - Adding a Users Slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
 import postsReducer from '@/features/posts/postsSlice'
+import usersReducer from '@/features/users/usersSlice'
 
 export const store = configureStore({
   // Pass in the root reducer setup as the `reducer` argument
   reducer: {
     // Declare that `state.counter` will be updated by the `counterReducer` function
-    posts: postsReducer
+    posts: postsReducer,
+    users: usersReducer
   }
 })
 
